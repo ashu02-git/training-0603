d1075134ed9b41fc6ac08cfe360fde483e8e778e
ashu02-git <shuhei.abe1107@gmail.com>
2021-06-04 15:00:16 +0900

    新規投稿Error時の表示追加、年齢マイナスもError

--- regist-user/middleware/inputValidator.js
+++ regist-user/middleware/inputValidator.js
@@ -1,15 +1,24 @@
 const inputValidator = (req, res, next) => {
   let nameLength = req.body.name.length;
-  let ageLength = req.body.age.length;
-  let error = true;
+  let age = req.body.age;
+  let nameFlag = false;
+  let ageFlag = false;
 
-  if (nameLength != 0 && ageLength != 0) {
-    error = false;
+  if (nameLength === 0) {
+    nameFlag = true;
   }
 
-  if (error) {
-    console.log('error');
-    res.render('add', { title: 'error' });
+  if ((age.length === 0) || (age < 0)) {
+    ageFlag = true;
+  } 
+
+  if (nameFlag || ageFlag) {
+
+    res.render('add', {
+      title: 'Error',
+      nameFlag: nameFlag,
+      ageFlag: ageFlag,
+    });
   } else {
     next();
   }
